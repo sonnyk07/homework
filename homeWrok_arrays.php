a2b7b848f676e65c2693357dca12e8e43df6e64e
sonnyk07 <83429187+sonnyk07@users.noreply.github.com>
2021-05-21 19:13:37 +0300

    Update homeWrok_arrays.php

--- homeWrok_arrays.php
+++ homeWrok_arrays.php
@@ -1,14 +1,14 @@
 <?php
 $menu = [
-    ['title' => 'Home'['link' => 'Http://homewrok/home.php']],
-    ['title' => 'Services'['link' =>'Http://homewrok/services.php']],
-    ['title' => 'Projects'['link' => 'Http://homewrok/projects.php']],
-    ['title' => 'Contacts'['link' => 'Http://homewrok/contacts.php',
-            'childTitle' => 'Phone' ['mobile'=>'079987389', 
-                            'stationare'=>'022-56632-566'],
-                'childTitle' => 'Fax'=>'022-56632-566',
-               'childTitle' => 'Email'=>'mySite']
-    ],
+    'title1' => 'Home','link1' => 'Http://homewrok/home.php',
+    'title2' => 'Services','link2' =>'Http://homewrok/projects.php',
+    'title3' => 'Projects','link3' => 'Http://homewrok/projects.php',
+    'title4' => 'Contacts','link4' => 'Http://homewrok/contacts.php',
+    'title5' =>  ['mobile' => '079987389',
+    'stationare'=>'022-56632-566']
 ];
+               
+
+
+
 var_dump($menu);
-var_dump($menu['3']['contactsMenu']['Phone']['mobile']);
