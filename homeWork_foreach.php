4d0df75bc90d3c3a6265854f77e9521ec2e653cd
sonnyk07 <83429187+sonnyk07@users.noreply.github.com>
2021-05-26 01:10:25 +0300

    Update homeWork_foreach.php

--- homeWork_foreach.php
+++ homeWork_foreach.php
@@ -1,11 +1,13 @@
+<?php
 $menu = [
     '<ul>'=>'<a href="home.php"><li>Home</li></a>',
     '<a href="services.php">'=>'<li>Services</li></a>',
     '<a href="projects.php">'=>'<li>Projects</li></a>',
-    '<a href="contcats.php">'=>'<ul>Contacts</a>',
+    '<a href="contcats.php">'=>'<ul><li>Contacts</li></a>',
     '<a href="phone.php">'=>'<li>Phone</li></a>',
     '<a href="fax.php">'=>'<li>FAX</li></a></ul></ul>'
     ];
 
 foreach ($menu as $key => $value){
     echo "$key : $value";
+    };
